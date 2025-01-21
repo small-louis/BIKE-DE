5fb7324cd9beafc878965b5058b75b112e15bd9d
small-louis <loet.d.s.brouwer@gmail.com>
2025-01-21 13:21:34 +0000

    Initial Commit

---
+++ BIKE-DE/BIKE_DEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIKE_DEApp.swift
+//  BIKE-DE
+//
+//  Created by Louis Brouwer on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BIKE_DEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
